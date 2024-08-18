d37308fbfaeb376a0219b04783dc9899b6d9e05b
arex123 <ad47kumar@gmail.com>
2024-08-18 21:44:32 +0530

    real time messaging using setinterval

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,70 +1,73 @@
-const serverURL = "http://localhost:4002"
-const button = document.getElementById('sendChat')
+const serverURL = "http://localhost:4002";
+const button = document.getElementById("sendChat");
 
-
-var btn = document.getElementById('message');
+var btn = document.getElementById("message");
 
 btn.onkeydown = function (e) {
-    var keyCode = e.keyCode || e.which;
-    if(keyCode==13) {
-     sendMessage()
-    }
+  var keyCode = e.keyCode || e.which;
+  if (keyCode == 13) {
+    sendMessage();
+  }
 };
 
-button.addEventListener('click',async ()=>sendMessage())
+button.addEventListener("click", async () => sendMessage());
 
-async function sendMessage(){
-
-    try{        
-        console.log("submit button clicked")
-        let new_message = document.getElementById('message').value
-        if(new_message.trim().length==0){
-            return
-        }
-        document.getElementById('message').value = ""
-        console.log("new message: ",new_message)
-        const token = localStorage.getItem('token')
-        let result  = await axios.post(serverURL+"/chat",{message:new_message},{
-            headers:{
-                'Authorization':token
-            }
-        })
-        getAllChats()
-        console.log("result ",result)
-    }catch(err){
-        console.log("error : ",err)
+async function sendMessage() {
+  try {
+    console.log("submit button clicked");
+    let new_message = document.getElementById("message").value;
+    if (new_message.trim().length == 0) {
+      return;
     }
-
+    document.getElementById("message").value = "";
+    console.log("new message: ", new_message);
+    const token = localStorage.getItem("token");
+    let result = await axios.post(
+      serverURL + "/chat",
+      { message: new_message },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    getAllChats();
+    console.log("result ", result);
+  } catch (err) {
+    console.log("error : ", err);
+  }
 }
 
-document.addEventListener("DOMContentLoaded", async() => getAllChats())
+document.addEventListener("DOMContentLoaded", async () => {
+  getAllChats();
+  setInterval(()=>{
+    getAllChats()
+  },1500)
+});
 
-async function getAllChats(){
-    let token = localStorage.getItem('token')
-    try{
+async function getAllChats() {
+  let token = localStorage.getItem("token");
+  try {
+    let result = await axios.get(serverURL + "/getChats", {
+      headers: {
+        Authorization: token,
+      },
+    });
 
-        let result = await axios.get(serverURL+"/getChats",{
-            headers:{
-                Authorization:token
-            }
-        })
-
-        console.log("Results",result)
-        let chat_data = result.data.messages
-        document.querySelector('.chat-list').innerHTML=''
-        for(let i=0;i<chat_data.length;i++){
-            ShowChatOnScreen(chat_data[i])
-        }
-
-    }catch(err){
-        console.log("erradf ",err)
+    console.log("Results", result);
+    let chat_data = result.data.messages;
+    document.querySelector(".chat-list").innerHTML = "";
+    for (let i = 0; i < chat_data.length; i++) {
+      ShowChatOnScreen(chat_data[i]);
     }
+  } catch (err) {
+    console.log("erradf ", err);
+  }
 }
-function ShowChatOnScreen(obj){
-
-    let chatCont = document.querySelector('.chat-list')    
-    let ptag = document.createElement('p')
-    ptag.textContent = obj.message
-    chatCont.appendChild(ptag)
-
+function ShowChatOnScreen(obj) {
+  let chatCont = document.querySelector(".chat-list");
+  let ptag = document.createElement("p");
+  ptag.textContent = obj.message;
+  chatCont.appendChild(ptag);
+  chatCont.scrollTop = chatCont.scrollHeight
 }
