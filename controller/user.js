73110a8799c3719218c4561a177ac12cfad5d8fd
arex123 <ad47kumar@gmail.com>
2024-08-18 17:17:56 +0530

    login complete

--- controller/user.js
+++ controller/user.js
@@ -1,27 +1,70 @@
 const User = require("../models/user");
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
+const Sequelize = require('sequelize')
 exports.register =async (req,res,next)=>{
     try{
         const {name, email, phone, password} = req.body;
         console.log("req.body :  ",req.body,name, email, phone, password)
 
-        const existingUser = await User.findOne({where: {email: email}});
+        const existingUser = await User.findOne({
+            where: {
+                [Sequelize.Op.or]: [
+                    { email: email },
+                    { phone: phone }
+                ]
+            }
+        });
         if(existingUser){
-            return res.status(400).json({message: "Email already exists."});
+            return res.status(400).json({success:false,message: "User with given Email or phone already exists."});
         }
 
         const hash = await bcrypt.hash(password, 10);
         await User.create({name: name, email: email, phone: phone, password: hash});
-        res.status(201).json({message: "User account created. \nPlease sign-in to continue"});
+        res.status(201).json({success:true,message: "User account created. \nPlease sign-in to continue"});
     }
     catch(err){
         console.log('SignUp-Error: ',err);
-        res.status(500).json({error: err, message: "something went wrong"})
+        res.status(500).json({success:false,error: err, message: "something went wrong"})
     }
 }
 
 exports.login = async(req,res,next)=>{
     let {email,password} = req.body
     console.log("req, body: ",email,password)
+    User.findAll({where:{email:email}}).then(user=>{
+        if(user.length==0){
+            res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }else{
+            user = user[0]
+            console.log("user from db ",user.password)
+
+            //comparing password with has in db
+            bcrypt.compare(password,user.password).then(result=>{
+                console.log("rsult ",result,"user.id: ",user.id)
+                if(result==false){
+                    res.status(401).json({
+                        success:false,
+                        message:"User Not Authorized"
+                    })
+                }else{
+
+                    //create jwt token now
+                    console.log("secret key ",process.env.tokenSecret)
+                    var token = jwt.sign({ id: user.id }, process.env.tokenSecret);
+                    res.status(200).json({
+                        success:true,
+                        message:"User is succesfuly logged in",
+                        token:token
+                    })
+
+                }
+            })
+
+
+        }
+    })
 }
--- public/js/index.js
+++ public/js/index.js
@@ -25,30 +25,30 @@ function handleSubmit(event) {
     .post(serverURL+"/user/register", data)
     .then((result) => {
       console.log(result);
-    //   if(result.data?.error){
-    //     messageTag.textContent = result.data?.error
-    //     messageTag.id = "failed"
-    //   }else{
-    //     messageTag.textContent = "Account created succesfully"
-    //     messageTag.id = "success"
-    //   }
+      if(result.data?.success==false){
+        messageTag.textContent = result.data?.message
+        messageTag.id = "failed"
+      }else{
+        messageTag.textContent = result.data?.message
+        messageTag.id = "success"
+      }
 
-    //   signupbuttontag.parentElement.appendChild(messageTag)
+      signupbuttontag.parentElement.appendChild(messageTag)
 
-    //   let time = setTimeout(()=>{
-    //     messageTag.remove()
-    //   },3000)
+      let time = setTimeout(()=>{
+        messageTag.remove()
+      },3000)
     })
     .catch(err => {
-      console.log("errr : ",err)
-    //   messageTag.textContent = "Something went wrong"
-    //   messageTag.id = "failed"
-    //   signupbuttontag.parentElement.appendChild(messageTag)
+      console.log("errr : ",err,err.response.data.message)
+      messageTag.textContent = err.response.data.message
+      messageTag.id = "failed"
+      signupbuttontag.parentElement.appendChild(messageTag)
 
       
-    //   let time = setTimeout(()=>{
-    //     messageTag.remove()
-    //   },3000)
+      let time = setTimeout(()=>{
+        messageTag.remove()
+      },3000)
     });
 }
 
